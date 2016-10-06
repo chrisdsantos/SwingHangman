ab5b6d9efaee108d99a8ef11e646c7b7508b88b4
nenayatzadeh <nenayatzadeh@cpp.edu>
2016-10-06 14:19:44 -0700

    Add files via upload

---
+++ src/main/java/HighScoreController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import main.java.SwingProject;
+import main.java.model.HighScoreModel;
+import main.java.view.HighScorePanel;
+/**
+ *
+ * @author Nahid
+ */
+public class HighScoreController {
+    private HighScorePanel panel;
+    private HighScoreModel model;
+    private MainFrameController rootController;
+    
+    public HighScoreController(HighScorePanel panel, HighScoreModel model, MainFrameController rootController) {
+        this.panel = panel;
+        this.model = model;
+        this.rootController = rootController;
+        
+        this.setup();
+    }
+    
+    private void setup() {
+        panel.setTitle(model.getHighScoreTitle());
+        panel.setMembers(model.getMembers());
+        panel.setBackgroundColor(model.getBackgroundColor());
+        panel.getBackButton().setText(model.getBackButtonText());
+        
+        panel.getBackButton().addActionListener(new ActionListener(){
+            
+         public void actionPerformed(ActionEvent e){
+               rootController.changeVisibleCard(SwingProject.FUNCTION_KEY);
+         }   
+        });
+    }
+    
+    public HighScorePanel getPanel() {
+        return this.panel;
+    }
+    
+    public void setPanel(HighScorePanel panel) {
+        this.panel = panel;
+    }
+    
+    public HighScoreModel getModel() {
+        return this.model;
+    }
+    
+    public void setModel(HighScoreModel model) {
+        this.model = model;
+    }
+
+}
